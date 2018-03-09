924224b0546a7f324aa7eb447db999ac68135288
Daniel Lim <daniel.jy.lim@gmail.com>
2018-03-09 23:59:21 +1100

    EDA of sample, convert dates with lubridate

---
+++ sample_eda.R
@@ -0,0 +1,8 @@
+setwd("E:/Analytics/Kaggle/Click Fraud/Data")
+
+# Read in data
+smp <- fread("train_sample.csv") %>% as.tibble
+
+# Adjust datetimes, read in as chr
+smp$click_time <- smp$click_time %>% ymd_hms
+smp$attributed_time <- smp$attributed_time %>% ymd_hms
