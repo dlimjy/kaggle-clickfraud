5b9ca24d649157610fef4428f509c5ecce2fb3ed
Daniel Lim <daniel.jy.lim@gmail.com>
2018-03-12 13:50:16 +1100

    feature creation initial

---
+++ feat_create.R
@@ -0,0 +1,5 @@
+setwd("E:/Analytics/Kaggle/Click Fraud/Data")
+
+# Read in both datasets
+train <- fread("train.csv")
+test <- fread("test.csv")
--- sample_eda.R
+++ sample_eda.R
@@ -6,3 +6,7 @@ smp <- fread("train_sample.csv") %>% as.tibble
 # Adjust datetimes, read in as chr
 smp$click_time <- smp$click_time %>% ymd_hms
 smp$attributed_time <- smp$attributed_time %>% ymd_hms
+
+# Feature generation
+smp$hr_of_day <- smp$click_time %>% hour # generate hour of day
+
